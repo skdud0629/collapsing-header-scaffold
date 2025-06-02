12816e04575317541d2b4c39e01f91db6c0048fe
skdud0629 <skdud4081@gmail.com>
2025-06-02 13:40:02 +0900

    Add: SystemBarInsets

---
+++ app/src/main/java/com/ny/collapsing/toolbar/SystemBarInsets.kt
@@ -0,0 +1,16 @@
+package com.ny.collapsing.toolbar
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.runtime.Composable
+
+object SystemBarInsets {
+    val windowInsets: WindowInsets
+        @Composable
+        get() =
+            WindowInsets.systemBars.only(
+                WindowInsetsSides.Horizontal + WindowInsetsSides.Top
+            )
+}
